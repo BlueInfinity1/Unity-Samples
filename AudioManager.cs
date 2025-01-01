5a5bed9cb73c0d14f22dfad363c9913fb6068b27
BlueInfinity1 <74124180+BlueInfinity1@users.noreply.github.com>
2025-01-02 00:13:20 +0200

    Update AudioManager.cs

--- AudioManager.cs
+++ AudioManager.cs
@@ -2,24 +2,31 @@ using System;
 using UnityEngine;
 using UnityEngine.Audio;
 
-//A singleton class for playing audio
+/* 
+This singleton class pools AudioSources to efficiently manage overlapping playback of sounds.
+Each sound needs a specific number of AudioSources assigned, depending on how many instances of that sound 
+might play simultaneously. For example, if a sound can overlap up to 3 times, you should assign 3 AudioSources.
+*/
+
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     public Sound[] music;
+
     [SerializeField] AudioMixer mainAudioMixer;
+    [SerializeField] bool playMusic; // Whether we should play background music or not when the level starts
+
     public static AudioManager instance;
-	
-    [SerializeField] private bool playMusic; //Whether we should play background music or not when the level starts
 
     void Awake()
     {
         if (instance == null)
-        {            
+        {
             DontDestroyOnLoad(gameObject);
             instance = this;
 
             int index = 0;
+
             foreach (Sound s in sounds)
             {
                 InitializeAudioSourcesForSound(s, index);
@@ -28,27 +35,38 @@ public class AudioManager : MonoBehaviour
 
             index = 0;
 
-            foreach (Sound s in music) //There shouldn't be a need to play bg themes overlappingly from many audiosources, so we'll use sources[0] for all of these
+            foreach (Sound s in music)
             {
+                // There shouldn't be a need to play background themes overlappingly from many audio sources,
+                // so we'll use sources[0] for all of these
                 InitializeAudioSourcesForSound(s, index);
                 index++;
             }
 
-            if (playMusic)            
+            if (playMusic)
+            {
                 Play(snd: Sound.LevelTheme, isLoopingSound: true, isSoundEffect: false);
-            
+            }
         }
-        else if (instance != this)
+        else
+        {
             Destroy(gameObject);
+        }
     }
 
+    /// <summary>
+    /// Initializes audio sources for a given sound.
+    /// </summary>
+    /// <param name="s">The sound object to initialize.</param>
+    /// <param name="soundIndex">The index of the sound.</param>
     void InitializeAudioSourcesForSound(Sound s, int soundIndex)
     {
-        s.baseIndex = soundIndex;        
+        s.baseIndex = soundIndex;
         s.sources = new AudioSource[s.totalSources];
 
-        for (int i = 0; i < s.totalSources; i++) //create multiple audiosources for the same sound if we need to play the sound many times overlappingly
+        for (int i = 0; i < s.totalSources; i++)
         {
+            // Create multiple audio sources for the same sound if overlapping playback is needed
             s.sources[i] = gameObject.AddComponent<AudioSource>();
             s.sources[i].clip = s.clip;
             s.sources[i].volume = s.volume;
@@ -57,6 +75,12 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Plays a sound or music track.
+    /// </summary>
+    /// <param name="snd">The sound to play.</param>
+    /// <param name="isLoopingSound">Whether the sound should loop.</param>
+    /// <param name="isSoundEffect">True if the sound is an effect, false if it's music.</param>
     public void Play(Sound snd, bool isLoopingSound = false, bool isSoundEffect = true)
     {
         string name = snd.Value;
@@ -65,8 +89,9 @@ public class AudioManager : MonoBehaviour
 
         if (s != null)
         {
+            // Pick the correct audio source to play, and pick a source that's most likely free
             AudioSource source = s.sources[s.currentSourceIndex];
-            s.currentSourceIndex = (s.currentSourceIndex + 1) % s.totalSources; //pick the correct audiosource to play, and pick a source that's most likely going to be free
+            s.currentSourceIndex = (s.currentSourceIndex + 1) % s.totalSources;
 
             if (source != null)
             {
@@ -75,28 +100,32 @@ public class AudioManager : MonoBehaviour
             }
         }
         else
+        {
             Debug.Log("Sound with name " + name + " could not be found");
+        }
     }
-	
-	/// <summary>
-	/// Sets the FX volume.
-	/// </summary>
-	/// <param name="volume">Volume.</param>
-	public void SetFXVolume(float volume)
-	{
-		//since we can't take the logarithm of 0, we need to change the value slightly when we're near 0
-		float nonZeroVolume = Mathf.Max(volume, 0.0001f);
-		mainAudioMixer.SetFloat("SoundVolume", Mathf.Log10(nonZeroVolume)*20);
-	}
-	
-	/// <summary>
-	/// Sets the music volume.
-	/// </summary>
-	/// <param name="volume">Volume.</param>
-	public void SetMusicVolume(float volume)
-	{
-		//since we can't take the logarithm of 0, we need to change the value slightly when we're near 0
-		float nonZeroVolume = Mathf.Max(volume, 0.0001f);
-		mainAudioMixer.SetFloat("MusicVolume", Mathf.Log10(nonZeroVolume)*20);
-	}
+
+    /// <summary>
+    /// Sets the FX volume on the main audio mixer.
+    /// Converts linear volume to a logarithmic scale.
+    /// </summary>
+    /// <param name="volume">Linear volume value (0 to 1).</param>
+    public void SetFXVolume(float volume)
+    {
+        // Since we can't take the logarithm of 0, we need to change the value slightly when near 0
+        float nonZeroVolume = Mathf.Max(volume, 0.0001f);
+        mainAudioMixer.SetFloat("SoundVolume", Mathf.Log10(nonZeroVolume) * 20);
+    }
+
+    /// <summary>
+    /// Sets the music volume on the main audio mixer.
+    /// Converts linear volume to a logarithmic scale.
+    /// </summary>
+    /// <param name="volume">Linear volume value (0 to 1).</param>
+    public void SetMusicVolume(float volume)
+    {
+        // Since we can't take the logarithm of 0, we need to change the value slightly when near 0
+        float nonZeroVolume = Mathf.Max(volume, 0.0001f);
+        mainAudioMixer.SetFloat("MusicVolume", Mathf.Log10(nonZeroVolume) * 20);
+    }
 }
