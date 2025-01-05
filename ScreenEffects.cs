b4234f9e0fb58f83858de4f345482ad2b9d8f9ff
BlueInfinity1 <74124180+BlueInfinity1@users.noreply.github.com>
2025-01-05 20:47:13 +0200

    Update ScreenEffects.cs

--- ScreenEffects.cs
+++ ScreenEffects.cs
@@ -2,41 +2,44 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-//This class will contain different camera-related effects, such as screen shakes, fade outs, glitch effects, etc.
-public class ScreenEffects : MonoBehaviour
+namespace VisualEffects
 {
-    [SerializeField] GameObject screenFadeVeil;
-    Image veilImage; 
-
-    void Start()
-    {        
-        screenFadeVeil.SetActive(false);
-        veilImage = screenFadeVeil.GetComponent<Image>();
-    }
-
-    //Initiate a screen fade in or out with the given fade speed
-    public void InitiateScreenFade(bool fadeOut = true, float fadeSpeed = 10.0f)
-    {        
-        StartCoroutine(FadeScreen(fadeOut, fadeSpeed));
-    }
-
-    IEnumerator FadeScreen(bool fadeOut, float fadeSpeed)
+    // This class will contain different camera-related effects, such as screen shakes, fade outs, glitch effects, etc.
+    public class ScreenEffects : MonoBehaviour
     {
-        screenFadeVeil.SetActive(true);
-        
-        Color veilColor = veilImage.color;
-        veilColor = new Color(veilColor.r, veilColor.g, veilColor.b, fadeOut ? 0 : 1);
-
-        int fadeMultiplier = fadeOut ? 1 : -1;
-        while (true)
+        [SerializeField] GameObject screenFadeVeil;
+        Image veilImage; 
+    
+        void Start()
+        {        
+            screenFadeVeil.SetActive(false);
+            veilImage = screenFadeVeil.GetComponent<Image>();
+        }
+    
+        // Initiate a screen fade in or out with the given fade speed
+        public void InitiateScreenFade(bool fadeOut = true, float fadeSpeed = 10.0f)
+        {        
+            StartCoroutine(FadeScreen(fadeOut, fadeSpeed));
+        }
+    
+        IEnumerator FadeScreen(bool fadeOut, float fadeSpeed)
         {
-            veilColor = new Color(veilColor.r, veilColor.g, veilColor.b, veilColor.a + fadeMultiplier * fadeSpeed * Time.deltaTime);
-            veilImage.color = veilColor;
-
-            if ((fadeOut && veilColor.a >= 1) || (!fadeOut && veilColor.a <= 0))
-                break;
-
-            yield return null;
+            screenFadeVeil.SetActive(true);
+            
+            Color veilColor = veilImage.color;
+            veilColor = new Color(veilColor.r, veilColor.g, veilColor.b, fadeOut ? 0 : 1);
+    
+            int fadeMultiplier = fadeOut ? 1 : -1;
+            while (true)
+            {
+                veilColor = new Color(veilColor.r, veilColor.g, veilColor.b, veilColor.a + fadeMultiplier * fadeSpeed * Time.deltaTime);
+                veilImage.color = veilColor;
+    
+                if ((fadeOut && veilColor.a >= 1) || (!fadeOut && veilColor.a <= 0))
+                    break;
+    
+                yield return null;
+            }
         }
     }
 }
